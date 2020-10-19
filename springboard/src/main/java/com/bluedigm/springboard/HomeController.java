9ed29d95097271329b5fb29199726819aba4cace
Zerunokasiar <woopower94@naver.com>
2020-10-19 16:25:15 +0900

    +mysql jdbc

--- springboard/src/main/java/com/bluedigm/springboard/HomeController.java
+++ springboard/src/main/java/com/bluedigm/springboard/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,24 +17,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "linkA")
+	public void linkA() {
+		logger.info("linkA called");
+	}
+
+	@RequestMapping(value = "linkB")
+	public void linkB() {
+		logger.info("linkB called");
+	}
+
+	@RequestMapping(value = "linkC")
+	public String linkC(@ModelAttribute("msg") String text) {
+		logger.info("linkC {} called".formatted(text));
+		return "result";
+	}
 }
